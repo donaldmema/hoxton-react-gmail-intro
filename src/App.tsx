9f573134f85734e5f47aabe5e333d0f82ccfab95
donaldmema <mema.donald@gmail.com>
2022-07-26 00:49:31 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import "./styles/Header.css";
+import "./styles/LeftNavMenu.css";
+import "./styles/EmailContent.css";
+
+import Header from "./components/Header";
+import LeftNavMenu from "./components/LeftNavMenu";
+import Main from "./components/Main";
+
+function App(): JSX.Element {
+  return (
+    <div className="app">
+      <Header />
+      <LeftNavMenu />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,17 @@
+// import "../styles/Main.css";
+// import "../styles/EmailContent.css";
+// import "../styles/EmailToolbar.css";
+
+import EmailContent from "./EmailContent";
+import EmailToolbar from "./EmailToolbar";
+
+function Main() {
+  return (
+    <>
+    <EmailToolbar /> 
+    <EmailContent />
+    </>
+  )
+}
+
+export default Main;
